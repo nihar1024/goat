67115b4246eaba9aefa759a153406133c7335265
EPajares <elias@plan4better.de>
2026-05-02 16:54:30 +0000

    feat(style): add COPY_STYLE/PASTE_STYLE enums, styleClipboard Redux slice, updateBaseLayerProperties API

--- apps/web/types/common/index.ts
+++ apps/web/types/common/index.ts
@@ -17,8 +17,10 @@ export enum ContentActions {
 
 export enum MapLayerActions {
   CHART = "chart",
+  COPY_STYLE = "copyStyle",
   DUPLICATE = "duplicate",
   EDIT_FEATURES = "editFeatures",
+  PASTE_STYLE = "pasteStyle",
   RENAME = "rename",
   ZOOM_TO = "zoomTo",
   PROPERTIES = "properties",
